5d14d3a1f91733e4f7ab54044a4e6d9b68481ba3
xxPicasso <ben@moregolf.co.nz>
2016-04-16 15:07:59 +1200

    initial commit. trying to add webpack

---
+++ js/objs/ref.js
@@ -0,0 +1,9 @@
+
+class Ref {
+  constructor(name, level) {
+    this.name = name;
+    this.level = level;
+  }
+}
+
+export class Ref
---
+++ js/script.js
@@ -0,0 +1,6 @@
+require('use-strict')
+
+import ref from './objs/ref'
+
+var r = new Ref("brian", 3)
+console.log("refs name: ", r.name)
