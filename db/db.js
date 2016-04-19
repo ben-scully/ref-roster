474fab109b78bccbc0e02f1f1650bd780060a6f7
xxPicasso <ben@moregolf.co.nz>
2016-04-19 17:57:54 +1200

    enough is enough

--- db/db.js
+++ db/db.js
@@ -3,5 +3,9 @@ var knex = require('./knexObj')
 module.exports = {
   getAll: function (tableName) {
     return knex.select().table(tableName)
+  },
+
+  isCompNameUnique: function (isDuplicate) {
+    return knex('competitions').select('cName').where(isDuplicate)
   }
 }
